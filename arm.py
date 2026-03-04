c07aba63470b51cf74820dd488b588414b08c003
Santa-jk <snehajoshyofficial@gmail.com>
2026-03-04 10:51:19 +0530

    Add files via upload

---
+++ arm.py
@@ -0,0 +1,19 @@
+# n = input("Enter a num:")
+# sum = 0
+# l = len(n)
+# for i in n:
+#     sum = sum + int(i) ** l
+# if(n==str(sum)):
+#     print("Number is armstrong")
+# else:
+#     print("Number is not armstrong")
+
+
+a=int(input('enter a num:'))
+b=int(input('enter anum:'))
+if(a>b):
+    print("hi")
+elif(a==b):
+    print('ok')
+else:
+    print("bye")
